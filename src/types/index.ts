14268d0b981fa120a70c1cfd0ee338626a9158ac
pavankumarkosaraju <pavankumarkosaraju@gmail.com>
2026-04-09 22:59:28 +0530

    Remove Node.js and TypeScript files, keep only Python project files

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './scraper.types';
-export * from './ocr.types';
-export * from './pipeline.types';
-export * from './polisher.types';
-export * from './narrator.types';
-export * from './tts.types';
-export * from './config.types';
